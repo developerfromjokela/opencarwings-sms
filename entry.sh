c62c75c1dbcee5b8abd0fae898f5a3d3da9e5aad
Developer From Jokela <developerfromjokela@gmail.com>
2026-09-14 16:56:42 +0300

    Add no GUI option to entry

--- entry.sh
+++ entry.sh
@@ -7,6 +7,6 @@ BAUD_RATE=$(jq --raw-output '.baud' /data/options.json)
 echo "Starting OpenCarwings Java SMS service on port: ${SERIAL_PORT} with baud rate: ${BAUD_RATE}"
 
 exec java -Djava.util.prefs.user_root=/data/java_prefs \
-          -jar /app/app.jar \
+          -jar /app/app.jar --no-gui \
           --serial-port "${SERIAL_PORT}" \
           --baud-rate "${BAUD_RATE}"
